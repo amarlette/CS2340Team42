42de3aee54cd4db40203bfc1f9b99da70230e767
Chidambaram Kadiresan <ckadiresan@outlook.com>
2018-01-22 17:57:21 -0500

    Revert "Added calc method to Person1"
    This reverts commit 7e74ddcfa031d4107a7aeda8c329afbc1859f6a8.

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,11 +30,8 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	    //Person 1 put your implementation here
-        if (input.length() > 2) {
-            return input.substring(2) + input.substring(0, 2);
-        }
-        return null;
+	  //Person 1 put your implementation here
+	  return null;
 	}
 	
 	/**
